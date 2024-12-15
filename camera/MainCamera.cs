be61d3be743d1d202b54a1fc29d27a1e0c1762f9
lukas.bajtek <60296261+bajteklukas@users.noreply.github.com>
2024-12-15 14:23:13 +0100

    zoom

--- camera/MainCamera.cs
+++ camera/MainCamera.cs
@@ -9,6 +9,23 @@ public partial class MainCamera : Camera2D
 	}
 
 	public override void _Process(double delta){
-		
+		if(Input.IsActionJustPressed("zoom_in")) {
+			ZoomIn();
+		}
+		if(Input.IsActionJustPressed("zoom_out")) {
+			ZoomOut();
+		}
+	}
+	
+	void ZoomIn(){
+		if(Zoom.X < 2) { 
+			Zoom += new Vector2(0.1f, 0.1f);
+		}
+	}
+	
+	void ZoomOut(){
+		if(Zoom.X > 1) { 
+			Zoom -= new Vector2(0.1f, 0.1f);
+		}
 	}
 }
