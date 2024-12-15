5823f114c41c11e72b7ad7a4c1c4a28902b893d7
lukas.bajtek <60296261+bajteklukas@users.noreply.github.com>
2024-12-15 15:26:54 +0100

    enemy

--- enemy/scripts/Enemy.cs
+++ enemy/scripts/Enemy.cs
@@ -3,8 +3,12 @@ using System;
 
 public partial class Enemy : Node2D
 {
+	CharacterBody2D playerObject;
+
+
 	public override void _Ready(){
-		
+		playerObject =  GetTree().Root.GetNode<Node2D>("main").GetNode<CharacterBody2D>("Player");
+		GD.Print(playerObject.Name);	
 	}
 
 	public override void _Process(double delta){
