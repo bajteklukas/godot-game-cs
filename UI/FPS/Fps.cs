16e08e2c16add4ba9bbe092a054324ee467b6aa5
lukas.bajtek <60296261+bajteklukas@users.noreply.github.com>
2024-12-16 14:10:17 +0100

    menu

--- UI/FPS/Fps.cs
+++ UI/FPS/Fps.cs
@@ -5,21 +5,16 @@ public partial class Fps : Control
 {
 	bool isPaused = false;
 
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta){
 		GetNode<Label>("Label").Text = $"{Engine.GetFramesPerSecond()}FPS";
 
-		if (Input.IsActionJustPressed("escape"))
-        {
+		if (Input.IsActionJustPressed("escape")){
             TogglePause();
         }
 	}
-	void TogglePause()
-    {
+	void TogglePause(){
         isPaused = !isPaused;
-
         Engine.TimeScale = isPaused ? 0.0f : 1.0f;
-
         GD.Print(isPaused ? "Game Paused" : "Game Resumed");
     }
 }
